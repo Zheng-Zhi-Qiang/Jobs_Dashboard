a5130223bee84ee051ecfb1ac98298277e985d39
Zheng Zhiqiang <you@example.com>
2023-01-08 21:41:18 +0800

    API request completed

---
+++ dashboard.py
@@ -0,0 +1,107 @@
+import os
+import pandas as pd
+from serpapi import GoogleSearch
+from IPython.display import display
+from nltk.tokenize import word_tokenize, MWETokenizer
+
+google_search_api_key = "292e7b0f85d63c22f169a196374dcf7d61ff0be7f7879be6613c55b0b2568215"
+search_term = "Data Analyst"
+search_location = "Singapore"
+
+params = {
+    "engine": "google_jobs",
+    "q": search_term,
+    "api_key": google_search_api_key,
+    "google_domain": "google.com",
+    "location": search_location,
+    "chips": "date_posted:today",
+    "start": 0,
+    "num": "100",
+    "no_cache": "true",
+}
+
+# Keywords to look out for
+keywords_programming = [
+    'sql', 'python', 'r', 'c', 'c#', 'javascript', 'js',  'java', 'scala', 'sas', 'matlab', 
+    'c++', 'c/c++', 'perl', 'go', 'typescript', 'bash', 'html', 'css', 'php', 'powershell', 'rust', 
+    'kotlin', 'ruby',  'dart', 'assembly', 'swift', 'vba', 'lua', 'groovy', 'delphi', 'objective-c', 
+    'haskell', 'elixir', 'julia', 'clojure', 'solidity', 'lisp', 'f#', 'fortran', 'erlang', 'apl', 
+    'cobol', 'ocaml', 'crystal', 'javascript/typescript', 'golang', 'nosql', 'mongodb', 't-sql', 'no-sql',
+    'visual_basic', 'pascal', 'mongo', 'pl/sql',  'sass', 'vb.net', 'mssql', 
+]
+
+keywords_libraries = [
+    'scikit-learn', 'jupyter', 'theano', 'openCV', 'spark', 'nltk', 'mlpack', 'chainer', 'fann', 'shogun', 
+    'dlib', 'mxnet', 'node.js', 'vue', 'vue.js', 'keras', 'ember.js', 'jse/jee',
+]
+
+keywords_analyst_tools = [
+    'excel', 'tableau',  'word', 'powerpoint', 'looker', 'powerbi', 'outlook', 'azure', 'jira', 'twilio',  'snowflake', 
+    'shell', 'linux', 'sas', 'sharepoint', 'mysql', 'visio', 'git', 'mssql', 'powerpoints', 'postgresql', 'spreadsheets',
+    'seaborn', 'pandas', 'gdpr', 'spreadsheet', 'alteryx', 'github', 'postgres', 'ssis', 'numpy', 'power_bi', 'spss', 'ssrs', 
+    'microstrategy',  'cognos', 'dax', 'matplotlib', 'dplyr', 'tidyr', 'ggplot2', 'plotly', 'esquisse', 'rshiny', 'mlr',
+    'docker', 'linux', 'jira',  'hadoop', 'airflow', 'redis', 'graphql', 'sap', 'tensorflow', 'node', 'asp.net', 'unix',
+    'jquery', 'pyspark', 'pytorch', 'gitlab', 'selenium', 'splunk', 'bitbucket', 'qlik', 'terminal', 'atlassian', 'unix/linux',
+    'linux/unix', 'ubuntu', 'nuix', 'datarobot',
+]
+
+keywords_cloud_tools = [
+    'aws', 'azure', 'gcp', 'snowflake', 'redshift', 'bigquery', 'aurora',
+]
+
+keywords_general_tools = [
+    'microsoft', 'slack', 'apache', 'ibm', 'html5', 'datadog', 'bloomberg',  'ajax', 'persicope', 'oracle', 
+]
+
+keywords_general = [
+    'coding', 'server', 'database', 'cloud', 'warehousing', 'scrum', 'devops', 'programming', 'saas', 'ci/cd', 'cicd', 
+    'ml', 'data_lake', 'frontend',' front-end', 'back-end', 'backend', 'json', 'xml', 'ios', 'kanban', 'nlp',
+    'iot', 'codebase', 'agile/scrum', 'agile', 'ai/ml', 'ai', 'paas', 'machine_learning', 'macros', 'iaas',
+    'fullstack', 'dataops', 'scrum/agile', 'ssas', 'mlops', 'debug', 'etl', 'a/b', 'slack', 'erp', 'oop', 
+    'object-oriented', 'etl/elt', 'elt', 'dashboarding', 'big-data', 'twilio', 'ui/ux', 'ux/ui', 'vlookup', 
+    'crossover',  'data_lake', 'data_lakes', 'bi', 
+]
+
+# Bind Multi-word tokens
+
+
+# Execute the search to retrieve the results
+for x in range(3):
+    start = x * 10
+    params["start"] = start
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    jobs = results["jobs_results"]
+    jobs_df = pd.DataFrame(jobs)
+    print(len(jobs_df.index))
+    jobs_df = pd.concat([jobs_df, pd.json_normalize(jobs_df["detected_extensions"])], axis=1).drop("detected_extensions", 1)
+    display(jobs_df)
+    print(len(jobs_df.index))
+    
+    if x == 0:
+        current_day_jobs_df = jobs_df
+    else:
+        current_day_jobs_df = current_day_jobs_df.append(jobs_df, ignore_index=True)
+
+# Convert the description column into a list
+description_list = current_day_jobs_df["description"].values.tolist()
+
+# Tokenize the strings to obtain list of tokens
+description_tokens = []
+
+for description in description_list:
+    description_tokens += word_tokenize(description.lower())
+
+# Remove punctuations from tokens
+
+# Recombine the tokens and retokenize them using MWETokenizer
+
+# Filter out stopwords
+
+# Filter out words that are not in keyword lists
+
+# Convert result list to dataframe
+
+# Group by and count the number of occurence for each unique word
+
+# Think about the metric to measure the demand
