9e3a82afa6345653c18f5b75a37690f92bd56b90
Zheng Zhiqiang <you@example.com>
2023-01-23 13:32:42 +0800

    created function

--- data_ingestion.py
+++ data_ingestion.py
@@ -1,9 +1,7 @@
-import re
 import pandas as pd
 import datetime
-import plotly.express as px
+import config
 from serpapi import GoogleSearch
-from nltk.tokenize import MWETokenizer
 from google.cloud import bigquery
 
 # Project Details
@@ -14,50 +12,53 @@ dataset_ref = bigquery.DatasetReference(project, dataset_id)
 # Current Date
 current_date = datetime.date.today()
 
-google_search_api_key = "2790b3e4171eab9e05796d210149d3b9db3f21e59baaac28a01edb107c4ae2a1"
-search_term = "Data Analyst"
-search_location = "Singapore"
+def data_analyst_ingestion():
+    """Ingest data analyst job data"""
 
-params = {
-    "engine": "google_jobs",
-    "q": search_term,
-    "api_key": google_search_api_key,
-    "google_domain": "google.com",
-    "location": search_location,
-    "chips": "date_posted:today",
-    "start": 0,
-    "num": "100",
-    "no_cache": "true",
-}
+    google_search_api_key = config.config_vars['google_search_api_key']
+    search_term = "Data Analyst"
+    search_location = "Singapore"
 
-# Execute the search to retrieve the results
-for x in range(3):
-    start = x * 10
-    params["start"] = start
-    search = GoogleSearch(params)
-    results = search.get_dict()
-    jobs = results["jobs_results"]
-    jobs_df = pd.DataFrame(jobs)
-    jobs_df = pd.concat([jobs_df, pd.json_normalize(jobs_df["detected_extensions"])], axis=1).drop("detected_extensions", 1)
-    if 'salary' in jobs_df.columns:
-        jobs_df = jobs_df.drop('salary', 1)
-    
-    if x == 0:
-        current_day_jobs_df = jobs_df
-    else:
-        current_day_jobs_df = current_day_jobs_df.append(jobs_df, ignore_index=True)
+    params = {
+        "engine": "google_jobs",
+        "q": search_term,
+        "api_key": google_search_api_key,
+        "google_domain": "google.com",
+        "location": search_location,
+        "chips": "date_posted:today",
+        "start": 0,
+        "num": "100",
+        "no_cache": "true",
+    }
 
-# Add date column to dataframe and drop related links and extensions
-current_day_jobs_df['date'] = pd.to_datetime(current_date)
-current_day_jobs_df = current_day_jobs_df.drop('related_links', 1).drop('extensions', 1)
+    # Execute the search to retrieve the results
+    for x in range(3):
+        start = x * 10
+        params["start"] = start
+        search = GoogleSearch(params)
+        results = search.get_dict()
+        jobs = results["jobs_results"]
+        jobs_df = pd.DataFrame(jobs)
+        jobs_df = pd.concat([jobs_df, pd.json_normalize(jobs_df["detected_extensions"])], axis=1).drop("detected_extensions", 1)
+        if 'salary' in jobs_df.columns:
+            jobs_df = jobs_df.drop('salary', 1)
+        
+        if x == 0:
+            current_day_jobs_df = jobs_df
+        else:
+            current_day_jobs_df = current_day_jobs_df.append(jobs_df, ignore_index=True)
 
-# Insert data into BigQuery
-client = bigquery.Client()
-table_ref = dataset_ref.table("raw_jobs")
-table = client.get_table(table_ref)
-errors = client.insert_rows_from_dataframe(table, current_day_jobs_df)
-if errors == []:
-    print(errors)
-    print('Upload failed')
-else: 
-    print('Data loaded into table')
+    # Add date column to dataframe and drop related links and extensions
+    current_day_jobs_df['date'] = pd.to_datetime(current_date)
+    current_day_jobs_df = current_day_jobs_df.drop('related_links', 1).drop('extensions', 1)
+
+    # Insert data into BigQuery
+    client = bigquery.Client()
+    table_ref = dataset_ref.table("raw_jobs")
+    table = client.get_table(table_ref)
+    errors = client.insert_rows_from_dataframe(table, current_day_jobs_df)
+    if errors == []:
+        print(errors)
+        print('Upload failed')
+    else: 
+        print('Data loaded into table')
