79e5fb40e38630ae4067324dfcfadef95f709c48
Zheng Zhiqiang <you@example.com>
2023-01-24 13:47:20 +0800

    created wordcloud for nonkey tokens

--- dashboard.py
+++ dashboard.py
@@ -138,10 +138,9 @@ nonkey_description_tokens = list(filter(lambda x: x not in keywords, description
 non_key += " ".join(nonkey_description_tokens) + " "
 
 # Create a wordcloud
-wordcloud = WordCloud(width = 800, height = 800,
+wordcloud = WordCloud(
                 background_color ='white',
-                stopwords = stop_words,
-                min_font_size = 10).generate(non_key)
+                stopwords = stop_words).generate(non_key)
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 
