492611b3d867138eb36795f53139a657550d8236
Zheng Zhiqiang <you@example.com>
2023-08-13 11:45:38 +0800

    added custom stopwords

--- dashboard.py
+++ dashboard.py
@@ -39,7 +39,7 @@ custom_stopwords = [
     'research', 'implement', 'function', 'science', 'based', 'assist', 'enable', 'employee', 'description', 'office',
     'hands', 'multiple', 'campaign', 'supporting', 'support', 'organisation', 'years', 'benefit', 'requirements',
     'least', 'review', 'work', 'engineer', 'u', 'risk', 'understanding', 'model', 'processes', 'process', 'ability', 'report',
-    'delivery', 'purpose', 'customer', 'closely', 'design', 'different', 'bachelor degree', 'employees', 
+    'delivery', 'purpose', 'customer', 'closely', 'design', 'different', 'employees', 'preferably', 'understand'
 ]
 for word in custom_stopwords:
     stop_words.add(word)
